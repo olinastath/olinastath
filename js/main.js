53bfe2e28a6c466ce743d570fb0c68fb8277a6c6
Olina Stathopoulou <olina.stath@nyu.edu>
2016-06-28 14:45:13 +0300

    changed typed to start w about page load

--- js/main.js
+++ js/main.js
@@ -4,6 +4,25 @@ $('#about').on('click',function() {
 	$('div.projects').fadeOut(1000);
 	$('div.press').fadeOut(1000);
 	$('div.about').fadeToggle(2500);
+
+	$(".i-like").typed({
+    strings: ["the internet.", "apple pie.", "intersectional feminism.", 
+    "cacti.", "theatre of all kinds.", "meta humor.", 
+    "complex female characters.", "Johnny Gallagher.", 
+    "the Greek Weird Wave.", "science.", "pop culture.", 
+    "YA novels about the disenchantment of youth.","sad indie rock.", 
+    "Crazy Ex-Girlfriend.", "the color green.", "Sylvia Plath.", 
+    "mashed potatoes.", "Community.",  "oreos.", "road trip movies.", 
+    "broccoli.", "Daphne." , "the color pink.", "twitter.",
+    "television.", "New York.", "concerts.", "fairy lights.", 
+    "tumblr poetry.", "Bo Burnham.", "cult movies."],
+    typeSpeed: 10,
+    showCursor: true,
+    loop: true,
+    loopCount: false,
+    startDelay: 3000,
+    backDelay: 1200,
+    });
 });
 
 $('#projects').on('click',function() {
@@ -54,25 +73,4 @@ $('#title').on('click',function() {
 	$('div.projects').fadeOut(1000);
 	$('div.press').fadeOut(1000);
 	$('div.contact').fadeOut(1000);
-});
-
-$(function(){
-    $(".i-like").typed({
-    strings: ["the internet.", "apple pie.", "intersectional feminism.", 
-    "cacti.", "theatre of all kinds.", "meta humor.", 
-    "complex female characters.", "Johnny Gallagher.", 
-    "the Greek Weird Wave.", "science.", "pop culture.", 
-    "YA novels about the disenchantment of youth.","sad indie rock.", 
-    "Crazy Ex-Girlfriend.", "the color green.", "Sylvia Plath.", 
-    "mashed potatoes.", "Community.",  "oreos.", "road trip movies.", 
-    "broccoli.", "Daphne." , "the color pink.", "twitter.",
-    "television.", "New York.", "concerts.", "fairy lights.", 
-    "tumblr poetry.", "Bo Burnham.", "cult movies."],
-    typeSpeed: 10,
-    showCursor: true,
-    loop: true,
-    loopCount: false,
-    startDelay: 5000,
-    backDelay: 1200,
-    });
 });
